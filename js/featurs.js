142d900ed993ae40401ea0224b4401fa231b7aa9
MistyIslam5 <mdmisty5@gmail.com>
2024-09-23 09:52:46 +0600

    history button clicked fucntionality added

--- js/featurs.js
+++ js/featurs.js
@@ -1,5 +1,27 @@
+const donationContainer = document.getElementById('donation-container');
+// console.log(donationContainer)
+
+
+
+
 const homeBtn = document.getElementById('home-btn');
 // console.log(homeBtn);
 homeBtn.addEventListener('click', function(){
     window.location.href = '/blog.html';
 })
+
+document.getElementById('donation-container-btn').addEventListener('click', function(){
+    donationContainer.classList.remove('hidden');
+    document.getElementById('donation-container-btn').classList.add('bg-[#B4F461]');
+    document.getElementById('history-btn').classList.remove('bg-[#B4F461]');
+})
+
+document.getElementById('history-btn').addEventListener('click', function(){
+    // console.log('clicked')
+    donationContainer.classList.add('hidden');
+    document.getElementById('donation-container-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-btn').classList.add('bg-[#B4F461]');
+
+    // console.log(donationContainer.classList)
+})
+
